bc522d98044892b4efeeae120326272cecf90c87
Fouad Salama <fs2030565@gmail.com>
2023-10-31 05:22:16 +0200

    change theme & add text to splash view body

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -13,6 +13,11 @@ class SplashViewBody extends StatelessWidget {
         Image.asset(
           AssetsData.logo,
         ),
+        const SizedBox(height: 4),
+        const Text(
+          'Reed Free Book',
+          textAlign: TextAlign.center,
+        ),
       ],
     );
   }
