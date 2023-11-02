54bb9b3e994d554709f308bca39d0917fd5473ff
Fouad Salama <fs2030565@gmail.com>
2023-11-02 03:09:05 +0200

    best seller list view item part one

--- lib/Features/home/presentation/views/home_view.dart
+++ lib/Features/home/presentation/views/home_view.dart
@@ -21,8 +21,40 @@ class HomeView extends StatelessWidget {
               'Best Seller',
               style: Styles.titleMedium,
             ),
+            BestSellerListViewItem(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: NetworkImage(
+                      'https://thumbs.dreamstime.com/b/old-book-flying-letters-magic-light-background-bookshelf-library-ancient-books-as-symbol-knowledge-history-218640948.jpg'),
+                ),
+              ),
+            ),
+          ),
+          Column(
+            children: [],
+          )
+        ],
       ),
     );
   }
