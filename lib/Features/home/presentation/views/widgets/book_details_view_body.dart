21cd531325f40e65c6fa2f6282c93925689a74f6
Fouad Salama <fs2030565@gmail.com>
2023-11-08 19:59:05 +0200

    create similar book list view

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -4,6 +4,7 @@ import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'books_action.dart';
 import 'custom_book_details_ap_bar.dart';
+import 'similar_books_list_view.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
@@ -39,6 +40,19 @@ class BookDetailsViewBody extends StatelessWidget {
           const BookRating(),
           const SizedBox(height: 37),
           const BookAction(),
+          const SizedBox(height: 50),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Text(
+              'You can also like',
+              style: Styles.textStyle14.copyWith(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          const SizedBox(height: 16),
+          const SimilarBooksListView(),
+          const SizedBox(height: 40),
         ],
       ),
     );
