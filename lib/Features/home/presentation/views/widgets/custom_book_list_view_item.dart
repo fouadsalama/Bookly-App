249a1ac9c15360a0bb950a0a2b5a7423b875b0b0
Fouad Salama <fs2030565@gmail.com>
2023-11-21 02:05:33 +0200

    cached network image

--- lib/Features/home/presentation/views/widgets/custom_book_list_view_item.dart
+++ lib/Features/home/presentation/views/widgets/custom_book_list_view_item.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
@@ -5,16 +6,18 @@ class CustomBookImage extends StatelessWidget {
   final String imageUrl;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
         aspectRatio: 2.6 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            image: DecorationImage(
-              fit: BoxFit.fill,
-              image: NetworkImage(imageUrl),
-            ),
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imageUrl,
+          errorWidget: (context, url, error) => const Icon(
+            Icons.error,
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
