0b5eb7f281af40a0588badcbb6f2b93c19e7d1b6
Fouad Salama <fs2030565@gmail.com>
2023-11-25 01:13:21 +0200

    create search cubit

--- lib/core/utils/manger/similar_books_cubit/similar_books_cubit.dart
+++ lib/core/utils/manger/similar_books_cubit/similar_books_cubit.dart
@@ -2,7 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:equatable/equatable.dart';
 
-import '../../../data/repos/home_repo.dart';
+import '../../../../Features/home/data/repos/home_repo.dart';
 
 part 'similar_books_state.dart';
 
